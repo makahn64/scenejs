23c8b82f816fe8b436027d11e44ca9e87a618270
atorres <torres.lyssa@gmail.com>
2015-07-31 13:08:20 -0700

    added start and stop zoom functions

--- simpleVisual/js/cameraFuncs.js
+++ simpleVisual/js/cameraFuncs.js
@@ -1,9 +1,9 @@
 /**
- * Functions to control/manipulate the trackball camera
+ * Functions to control/manipulate the trackball camera.
  */
 function startOrbit() {
     scene.getNode("camera", function(camera) {
-        camera._tick = scene.on("tick", function() {
+        camera._tickOrbit = scene.on("tick", function() {
             camera.addRotation({x:0, y:1, z:0, angle:0.1});
         });
     });
@@ -11,6 +11,20 @@ function startOrbit() {
 
 function stopOrbit() {
     scene.getNode("camera", function(camera) {
-        scene.off(camera._tick);
+        scene.off(camera._tickOrbit);
+    });
+}
+
+function startZoom() {
+    scene.getNode("camera", function(camera) {
+        camera._tickZoom = scene.on("tick", function() {
+            camera.addZoom(0.1);
+        });
+    });
+}
+
+function stopZoom() {
+    scene.getNode("camera", function(camera) {
+        scene.off(camera._tickZoom);
     });
 }
