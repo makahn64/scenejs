54d0d5f0cbfeab8a355eaa2f419f3ec4ccd1df7b
atorres <torres.lyssa@gmail.com>
2015-07-31 10:03:45 -0700

    updated simpleVisual project to most recent version

--- simpleVisual/plugins/node/models/space/planets/cartoonearth.js
+++ simpleVisual/plugins/node/models/space/planets/cartoonearth.js
@@ -10,15 +10,16 @@ SceneJS.Types.addType("models/space/planets/cartoonearth", {
         });
 
         // Used to rotate the earth around its y-axis
-        var earthRotate = node = node.addNode({
+        var earthRotate = node.addNode({
             type:"rotate",
             id:"earthRotate",
             y:1
         });
 
-        // Layer 0: Earth's land with color
-        node = node.addNode({
+        // Layer 0: Earth's land
+        earthRotate.addNode({
             type:"layer",
+            id:"land",
             priority:0,
 
             nodes:[{
@@ -39,8 +40,9 @@ SceneJS.Types.addType("models/space/planets/cartoonearth", {
         });
 
         // Layer 1: Earth's oceans with color
-        node = node.addNode({
+       earthRotate.addNode({
             type:"layer",
+            id:"water",
             priority:1,
 
             nodes:[{
@@ -75,34 +77,20 @@ SceneJS.Types.addType("models/space/planets/cartoonearth", {
         });
 
         //Layer 2: People
-        node = node.addNode({
+        earthRotate.addNode({
             type:"layer",
             id:"peopleLayer",
-            priority:2,
-
-            nodes:[{
-                type:"translate",
-                z:2.03,
-
-                nodes:[{
-                    type: "material",
-                    color: {r: 1.0, g: 0, b: 0},
-
-                    nodes: [{
-                        type: "geometry/plane",
-                        width: 0.2, height: 0.2
-                    }]
-                }]
-            }]
+            priority:2
         });
 
         var earthAngle = 0;
 
-        this._tick = this.getScene().on("tick", function() {
-            earthRotate.setAngle(earthAngle);
-
-            earthAngle -= 0.05;
-        });
+        /*this._tick = this.getScene().on("tick", function() {
+            if (params.rotate) {
+                earthRotate.setAngle(earthAngle);
+                earthAngle -= 0.05;
+            }
+        });*/
     },
 
     // Node deconstructor unsubscribes from scene tick
