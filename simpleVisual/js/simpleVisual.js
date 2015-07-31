d84a290f02333d453ddeb8dc858ee5b3cac2a0a4
atorres <torres.lyssa@gmail.com>
2015-07-31 09:59:05 -0700

    updated simpleVisual.js to most recent version

--- simpleVisual/js/simpleVisual.js
+++ simpleVisual/js/simpleVisual.js
@@ -6,41 +6,52 @@ SceneJS.setConfigs({
 
 var scene = SceneJS.createScene({
     nodes:[{
-        type: "cameras/earthPickFly",
-        id: "myCamera",
-        showCursor:true,
-        cursorSize:0.1,
-        zoom:-10,
+        type: "lights",
+        id: "myLights",
+        lights: [{
+            mode: "ambient",
+            color: {r: 1.0, g: 1.0, b: 1.0}
+        }],
 
-        nodes:[{
-            type: "lights",
-            id: "myLights",
-            lights: [{
-                mode: "ambient",
-                color: {r: 1.0, g: 1.0, b: 1.0}
-            }],
+        nodes: [{
+            type:"cameras/orbit",
+            id:"camera",
+            zoom:10,
+            zoomSensitivity:1,
+            spin:0.1,
 
-            nodes: [{
+            nodes:[{
                 type: "translate",
-                x: 2, y: 1,
-                id:"earth",
+                x:2, y: 1,
+                id: "earth",
 
                 nodes: [{
                     type: "models/space/planets/cartoonearth",
+                    id:"earthModel",
+                    rotate:true
                 }]
-            }, {
-                type: "translate",
-                id: "personNode",
-                x: -5, y:0, z:0,
+            }]
+        }, {
+            type: "translate",
+            id: "personNode",
+            x: -5, y:0, z:0,
+
+            nodes:[{
+                type:"scale",
+                id:"personScale",
+                x:1, y:1, z:1
             }]
         }]
     }]
 });
 
+/* Position the person and earth model at center */
 
 window.setTimeout(function() {
-    scene.getNode("personNode", function(personNode) {
-        personNode.addNode({
+    scene.getNode("personScale", function(personScale) {
+        // Add image to personNode
+        // TODO: add text stuff
+        personScale.addNode({
             type: "texture",
             src: "img/face.jpg",
             nodes: [{
@@ -48,20 +59,56 @@ window.setTimeout(function() {
                 width: 1, height: 1
             }]
         });
-        var x = -5, y = 0, z = 0;
-        var inc = 0.05;
+    });
+    scene.getNode("personNode", function(personNode) {
+        // TODO: make movements more fluid, maybe using tween.js
+        // Move person to center
+        var x = -5, z = 0;
+        var inc = 0.04;
         scene.on("tick", function() {
-            if (x < 2) {
-                x += inc+0.03;
-            }
-            if (y < 1) {
-                y += 0.02;
-            }
-            if (z < 2.03) {
+            if (x < 0)
+                x += inc;
+            if (z < 2.04)  // to keep image in front of earth
                 z += inc;
-            }
-            personNode.set({x:x, y:y, z:z});
+            personNode.set({x:x, z:z});
         });
+    });
+
+    scene.getNode("earth", function(earth) {
+        // Move earth to center
+        var x = 2, y = 1;
+        var inc = 0.02;
+        scene.on("tick", function() {
+            if (x > 0)
+                x -= inc;
+            if (y > 0)
+                y -= inc;
+            earth.set({x:x, y:y});
+        });
+    });
+
+    scene.getNode("camera", function(camera) {
+        // TODO: Stop earth from rotating and spin to desired angle
+        //scene.off(camera._tick);
     });
 }, 2000);
 
+/* Place person in earth layer */
+
+window.setTimeout(function() {
+    scene.getNode("peopleLayer", function(peopleLayer) {
+        scene.getNode("personNode", function(personNode) {
+            scene.getNode("personScale", function(personScale) {
+                personScale.set({x:-0.2, y:-0.2, z:0.2});
+            });
+            personNode.disconnect();
+            peopleLayer.addNode(personNode);
+            /*var x;
+            scene.on("tick", function() {
+                if (x < 0)
+                    x += 0.02;
+                personNode.setX(x);
+            });*/
+        });
+    });
+}, 4000);
