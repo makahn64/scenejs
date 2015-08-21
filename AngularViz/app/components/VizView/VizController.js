043e2e30addadadae05d01a8faa3dbadb17e3375
atorres <torres.lyssa@gmail.com>
2015-08-21 12:24:21 -0700

    viz view added

---
+++ AngularViz/app/components/VizView/VizController.js
@@ -0,0 +1,11 @@
+app.controller("vizViewController", [
+    '$scope', '$log',
+    function ( $scope, $log ) {
+
+        $log.debug("Loading vizViewController");
+
+        //userDefaults.setStringForKey('hello', 'Ahoy matey!');
+        //$log.debug("RC: "+userDefaults.getStringForKey('hello'));
+
+    }
+]);
